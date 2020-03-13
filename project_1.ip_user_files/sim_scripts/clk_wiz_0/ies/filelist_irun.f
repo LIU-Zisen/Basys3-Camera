1b3c4d75c3d3b3d4f4d749c336c32d18b0987d51
liuzs <liuzs@mail2.sysu.edu.cn>
2020-03-13 09:01:44 +0800

    remove extra files

--- project_1.ip_user_files/sim_scripts/clk_wiz_0/ies/filelist_irun.f
+++
@@ -1,8 +0,0 @@
--makelib ies/xil_defaultlib \
-  "../../../../project_1.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
-  "../../../../project_1.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.v" \
--endlib
--makelib ies/xil_defaultlib \
-  glbl.v
--endlib
-
--- project_1.ip_user_files/sim_scripts/clk_wiz_0/modelsim/filelist.f
+++
@@ -1,3 +0,0 @@
-../../../../project_1.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v
-../../../../project_1.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.v
-glbl.v
